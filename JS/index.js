a53452cad58bfea06d01370fcd3f747d1b2c4100
vvalugonzalez <valu322@gmail.com>
2022-08-18 21:37:06 -0300

    Ciclos

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+//Consultar cierta cantidad de clientes por nombre
+let cliente = (prompt("Ingrese nombre del cliente"))
+let numero = prompt("Ingrese cantidad de clientes a mostrar con ese nombre")
+
+for(i=1; i<= numero; i++){
+    console.log("Cliente " + i + " " + cliente + ".....")
+    
+}
+
